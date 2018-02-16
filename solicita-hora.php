1d2b5d28f814eb07e48bf73f613be2172816da58
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2018-02-15 22:56:26 -0500

    Actualiza el formulario de registro de hora.

--- solicita-hora.php
+++ solicita-hora.php
@@ -1,43 +1,67 @@
 <div class="container">
-    <div class="row">
-        <form class="form-horizontal">
+    <div class="col-md-12 contact-form">
+        <<form class="form">
             <div class="row">
-                <div class="form-group col-md-3">
-                    <label for="inputEmail3">Correo Electrónico</label>
-                    <input type="email" class="form-control" id="inputEmail" placeholder="Correo">
-                </div>
-                <div class="form-group col-md-3">
-                    <label for="inputPhone">Teléfono</label>
-                    <input type="tel" class="form-control" id="inputPhone" placeholder="Teléfono">
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-group col-md-2">
-                    <label for="paterno" >Apellido Paterno</label>
-                    <input type="text" class="form-control" id="paterno" placeholder="Apellido Paterno">
-                </div>
-
-                <div class="form-group col-md-2">
-                    <label for="materno" >Apellido Mterno</label>
-                    <input type="text" class="form-control" id="materno" placeholder="Apellido Materno">
-                </div>
-                <div class="form-group col-md-2">
-                    <label for="nombres">Nombres</label>
-                    <input type="text" class="form-control" id="nombres" placeholder="Nombres">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-offset-2">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> Remember me
-                        </label>
+                <div class="form-group col-md-5 has-danger">
+                    <label for="inputEmail" class="col-md-2 col-form-label">Email</label>
+                    <div class="col-md-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+                            <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm">
-                    <button type="submit" class="btn btn-default">Sign in</button>
+                <div class="form-group col-md-5">
+                    <label for="inputPhone" class="col-md-2 col-form-label">Teléfono</label>
+                    <div class="col-md-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-phone-alt"></i></span>
+                            <input type="tel" class="form-control" name="inputPhone" id="inputPhone" placeholder="Teléfono">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">                
+                <div class="form-group col-md-5 has-danger">
+                    <label for="paterno" class="col-md-2 col-form-label">Apellido Paterno</label>
+                    <div class="col-md-10">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" class="form-control form-control-danger" name="paterno" id="paterno" placeholder="Apellido Paterno">
+                            <div class="form-control-feedback">Debe ingresar el apellido</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="form-group col-md-5">
+                    <label for="materno" class="col-md-2 col-form-label">Apellido Materno</label>
+                    <div class="col-md-10">              
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" class="form-control" id="materno" placeholder="Apellido Materno">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-5 has-danger">
+                    <label for="nombres" class="col-md-2 col-form-label">Nombre</label>
+                    <div class="col-md-10">                     
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input type="text" class="form-control form-control-danger" id="nombres" placeholder="Nombre">
+                            <div class="form-control-feedback">Debe ingresar el nombre</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    <input class="submit-btn" type="submit" value="ENVIAR">
                 </div>
             </div>
         </form>
